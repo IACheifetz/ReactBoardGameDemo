32398c7c1201d573969c6366ec34605976c054af
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-11 14:02:56 -0700

    declared state and imported usehistory in createpage js

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { createGame } from './services/fetch-utils';
+import { useHistory } from 'react-router-dom';
 
 export default function CreatePage() {
   // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
@@ -11,13 +12,29 @@ export default function CreatePage() {
     // description;
     // minPlayers;
     // maxPlayers;
+  const history = useHistory();
+  const [title, setTitle] = useState('');
+  const [genre, setGenre] = useState('');
+  const [designer, setDesigner] = useState('');
+  const [description, setDescription] = useState('');
+  const [minPlayers, setMinPlayers] = useState(0);
+  const [maxPlayers, setMaxPlayers] = useState(0);
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     // create a game
+    await createGame({
+      title,
+      genre,
+      designer,
+      description,
+      min_players: minPlayers,
+      max_players: maxPlayers
+    });
 
     // use history.push to send the user to the list page
+    history.push('/board-games');
   }
 
   return (
